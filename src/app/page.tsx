47010cb6d07ce9714d08c94a4279a59b7986611c
Jomariel Gaitera <jojogaitera@gmail.com>
2024-06-18 01:10:32 +0800

    Add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main className='m-4 max-w-screen-sm mx-4 md:mx-auto'>
       <h1 className='font-extrabold text-2xl'>Hello, World!</h1>
-      <p className=' my-4 text-gray-500 bg-secondary p-4 rounded'>
+      <p className='my-4 text-gray-500 bg-secondary p-4 rounded'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
         debitis sunt ex totam mollitia excepturi dolor, dolore quidem officiis
         aperiam laboriosam! Nesciunt obcaecati dicta blanditiis architecto
